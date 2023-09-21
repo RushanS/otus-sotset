452e29545fdd786093afac2169c2d90101838439
Rushan <r.seyfetdinov@tinkoff.ru>
2023-09-21 13:29:57 +0300

    add startup instructions, add postman collection

---
+++ src/test/kotlin/ru/otus/sotset/api/ApplicationTest.kt
@@ -0,0 +1,13 @@
+package ru.otus.sotset.api
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ApplicationTest {
+
+    @Test
+    fun contextLoads() {
+        println("Context loaded")
+    }
+}
